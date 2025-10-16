f60a03db26f1e5b5f8d0f103a948579e3b7587cb
biggestbonk <harry.walsh505@gmail.com>
2025-10-16 16:43:28 +1300

    fix broken merge

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,6 +9,25 @@ import { AuthError } from 'next-auth'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.'
+        default:
+          return 'Something went wrong.'
+      }
+    }
+    throw error
+  }
+}
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -108,23 +127,4 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`
   revalidatePath('/dashboard/invoices')
-}
-
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData
-) {
-  try {
-    await signIn('credentials', formData)
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials'
-        default:
-          return 'Something went wrong'
-      }
-    }
-    throw error
-  }
 }
