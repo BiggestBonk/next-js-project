7e44e62dccbdff24d5477cf6aff64856f37abac9
biggestbonk <harry.walsh505@gmail.com>
2025-10-17 18:19:32 +1300

    add newline for connectionstring

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,8 @@
 import postgres from 'postgres'
 
-const connectionString = 'postgresql://neondb_owner:npg_Wmqu9Dv0tGTQ@ep-morning-sunset-a7207zvq.ap-southeast-2.aws.neon.tech/neondb?sslmode=require'
+const connectionString =
+  'postgresql://neondb_owner:npg_Wmqu9Dv0tGTQ@ep-morning-sunset-a7207zvq.ap-southeast-2.aws.neon.tech/neondb?sslmode=require'
 
-console.log('Connecting to:', connectionString.split('@')[1]) 
+console.log('Connecting to:', connectionString.split('@')[1])
 
 export const sql = postgres(connectionString, { ssl: 'require' })
