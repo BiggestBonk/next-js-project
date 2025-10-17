f363f1c42d93453dddb3f4ae5245639ed688129f
biggestbonk <harry.walsh505@gmail.com>
2025-10-17 16:22:26 +1300

    hardcode connectionString

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,9 +1,8 @@
 import postgres from 'postgres'
 
-console.log('=== DB Connection Debug ===')
-console.log('POSTGRES_URL env var:', process.env.POSTGRES_URL)
-console.log('POSTGRES_URL type:', typeof process.env.POSTGRES_URL)
-console.log('POSTGRES_URL length:', process.env.POSTGRES_URL?.length)
-console.log('========================')
+const connectionString =
+  'postgresql://neondb_owner:npg_Wmqu9Dv0tGTQ@ep-morning-sunset-a7207zvq.ap-southeast-2.aws.neon.tech/neondb?sslmode=require'
 
-export const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
+console.log('Connecting to:', connectionString.split('@')[1])
+
+export const sql = postgres(connectionString, { ssl: 'require' })
