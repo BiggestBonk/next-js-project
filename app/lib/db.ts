859cbd2b7d365bd9a8642f97e0f574b50672f56b
biggestbonk <harry.walsh505@gmail.com>
2025-10-17 16:02:44 +1300

    further debug logs

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,3 +1,9 @@
 import postgres from 'postgres'
 
+console.log('=== DB Connection Debug ===')
+console.log('POSTGRES_URL env var:', process.env.POSTGRES_URL)
+console.log('POSTGRES_URL type:', typeof process.env.POSTGRES_URL)
+console.log('POSTGRES_URL length:', process.env.POSTGRES_URL?.length)
+console.log('========================')
+
 export const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
