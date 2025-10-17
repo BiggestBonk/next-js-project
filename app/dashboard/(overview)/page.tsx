1156d7ab3b1d96f54ea9148eb427cc4b5b15f044
biggestbonk <harry.walsh505@gmail.com>
2025-10-17 18:01:32 +1300

    add back hardcoded url

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,8 +10,6 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons'
 
-export const dynamic = 'force-dynamic'
-
 export default async function Page() {
   const {
     numberOfInvoices,
--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,3 +1,7 @@
 import postgres from 'postgres'
 
-export const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
+const connectionString = 'postgresql://neondb_owner:npg_Wmqu9Dv0tGTQ@ep-morning-sunset-a7207zvq.ap-southeast-2.aws.neon.tech/neondb?sslmode=require'
+
+console.log('Connecting to:', connectionString.split('@')[1]) 
+
+export const sql = postgres(connectionString, { ssl: 'require' })
