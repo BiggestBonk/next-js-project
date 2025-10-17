0253e47bff9a09f7ca53ef98f3177ead4637a743
biggestbonk <harry.walsh505@gmail.com>
2025-10-17 17:14:30 +1300

    replaced harcoded value

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,3 @@
 import postgres from 'postgres'
 
-const connectionString =
-  'postgresql://neondb_owner:npg_Wmqu9Dv0tGTQ@ep-morning-sunset-a7207zvq.ap-southeast-2.aws.neon.tech/neondb?sslmode=require'
-
-console.log('Connecting to:', connectionString.split('@')[1])
-
-export const sql = postgres(connectionString, { ssl: 'require' })
+export const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
